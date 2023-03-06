ee260167c42530a784e2e73a0de163a957e7ca5a
Hüseyin Çınar <84516708+Huseyin-Cinar@users.noreply.github.com>
2023-03-06 15:34:07 +0300

    Update userinfo.js

--- Constants/userinfo.js
+++ Constants/userinfo.js
@@ -3,70 +3,70 @@
 import { faGithubAlt, faLinkedinIn, faMediumM, faFacebook, faTwitter, faInstagram, faYoutube, faBehance } from "@fortawesome/free-brands-svg-icons"
 
 export const userinfo = {
-    logoText: "Michael Scott", //This text is visible on your navbar and footer like your logo.
+    logoText: "Huseyin Cinar", //This text is visible on your navbar and footer like your logo.
     contact: {
-        email: 'michaelscott@email.com', //It is always a good idea to mention your email on your website. Good platform to communicate.
-        phone: '1234567890', //Phone number is optional, if you dont want it, consider leaving it blank .
-        countrycode: '+91' //It is advisable to add the country code incase you mention your contact number.
+        email: 'huseyinncinar0@gmail.com', //It is always a good idea to mention your email on your website. Good platform to communicate.
+        phone: '555 555 55 55', //Phone number is optional, if you dont want it, consider leaving it blank .
+        countrycode: '+90' //It is advisable to add the country code incase you mention your contact number.
     },
     socials: [
         //For aesthetics, it is advisable for you to mention upto 4 social media links only. Fill in the links.
         //more icons are imported above, use as you like them.
-        { type: 'github', link: '', icon: faGithubAlt },
-        { type: 'linkedin', link: '', icon: faLinkedinIn },
-        { type: 'medium', link: '', icon: faMediumM },
+        { type: 'github', link: 'https://github.com/Huseyin-Cinar', icon: faGithubAlt },
+        { type: 'linkedin', link: 'https://linkedin.com/huseyincinar', icon: faLinkedinIn },
+        { type: 'medium', link: 'https://medium.com/huseyincinar', icon: faMediumM },
     ],
     greeting: {
         //this text goes on your landing page
-        title: "Hey there, I am Michael.",
-        subtitle: "Michael Gary Scott is a fictional character in NBC's The Office, portrayed by Steve Carell. Michael is the protagonist of the series, serving as the Regional Manager of the Scranton branch of a paper distribution company, Dunder Mifflin Inc. Also, that's what she said!"
+        title: "Hey there, I am Huseyin.",
+        subtitle: "I'm 16 years old Self-taught Front-end Developer and I most improve myself on Cascading Style Sheets. I love it." 
     },
     capabilities: [
         //this text goes on your home page in table format.
         //For aesthetics, it is advisable for you to mention upto 4 catgeories links only.
         //For aesthetics, it is advisable for you to keep similar number of skills in each category
         {
-            category: "Category", // eg.frontend, backend, devops etc
-            skills: ["Skillset1", "Skillset1", "Skillset1"] //eg. react, html, python etc.
+            category: "frontend", // eg.frontend, backend, devops etc
+            skills: ["html", "css", "phyton"] //eg. react, html, python etc.
         },
         {
-            category: "Category",
-            skills: ["Skillset2", "Skillset2", "Skillset2"]
+            category: "backend",
+            skills: ["javascript", "typescript", "php"]
+        },
+        { altyapı
+            category: "discord.js",
+            skills: ["v14 Bot", "Server Template", "Music Infrastructure"]
         },
         {
-            category: "Category",
-            skills: ["Skillset3", "Skillset3", "Skillset3"]
-        },
-        {
-            category: "Category",
-            skills: ["Skillset4", "Skillset4", "Skillset4"]
+            category: "node.js",
+            skills: ["CentOS", "My SQL", "Chat App"]
         },
     ],
     about: {
         //this text goes at the bottom of your home page.
-        content: "'Do I need to be liked? Absolutely not. I like to be liked. I enjoy being liked. I have to be liked. But it’s not like this compulsive need like my need to be praised.' Whatever the situation or conflict might be, it can almost always be traced back to this need. His conference room characters, fun runs, and copious amounts of office parties are all aimed at his need to be praised.",
-        resume: "/" //link your resume here. it can be drive link or any other link
+        content: "Hi, I'm 16 years old Self-taught Front-end Developer and I most improve myself on Cascading Style Sheets. I am working on <a href='https://github.com/hackclub' target='_blank'>Hack Club</a> and <a href='https://github.com/thefuture-org' target='_blank'>the Future</a> from Home in Turkiye. I can speaking English, Turkish and a little bit German. I stopped making Discord bots but I still do to special requests. You can find all my links for advertising and collaborations <a href='https://futuree.netlify.app/feedback' target='_blank'>here</a>. Currently, I am website creating with HTML, CSS and Javascript. I think that's all, if you like me you can follow me on <a href='https://github.com/Huseyin-Cinar' target='_blank'>Github</a>.",
+        resume: "https://futuree.netlify.app/en" //link your resume here. it can be drive link or any other link
     },
     education: {
         visible: true, //set this to false if you want to omit this section
         educationList: [
             {
                 time: 'May - Oct 2020', //timespan
-                title: 'Degree name', //eg. BTech in Compuster Engineering
-                organization: 'School Name', //eg. VJTI, Mumbai
-                description: 'Lorem ipsum dolor sit amet consectetur' //eg. secured a cgpa of 9.25 in all sems appeared in so far
+                title: 'Student', //eg. BTech in Compuster Engineering
+                organization: 'Seventy-fifth year Middle School', //eg. VJTI, Mumbai
+                description: 'secured a diploma score of 100 in all sems appeared in so far' //eg. secured a cgpa of 9.25 in all sems appeared in so far
             },
             {
-                time: 'May - Oct 2020',
-                title: 'Degree name',
-                organization: 'School Name',
-                description: 'Lorem ipsum dolor sit amet consectetur'
+                time: 'May - Oct 2021',
+                title: 'Student',
+                organization: 'Farabi Anatolian High School',
+                description: 'secured a diploma score of 97.9 in all sems appeared in so far'
             },
             {
-                time: 'May - Oct 2020',
-                title: 'Degree name',
-                organization: 'School Name',
-                description: 'Lorem ipsum dolor sit amet consectetur'
+                time: 'May - Oct 2022',
+                title: 'Student',
+                organization: 'Farabi Anatolian High School',
+                description: 'secured a diploma score of 99.1 in all sems appeared in so far'
             },
         ],
     },
@@ -74,25 +74,18 @@ export const userinfo = {
         visible: true, //set this to false if you want to omit this section
         experienceList: [
             {
-                company: 'Dunder Mifflin', //company name eg.Microsoft
-                companylogo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*', //companylogo
-                position: 'Regional Manager', //post you held eg.Senior SDE
-                time: 'March 2020 - May 2020', //timespan
-                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum' //eg. spearheaded the graphics team in designing SASS product UIs
+                company: 'Hack Club', //company name eg.Microsoft
+                companylogo: 'https://avatars.githubusercontent.com/u/5633654?v=4', //companylogo
+                position: 'Contributor', //post you held eg.Senior SDE
+                time: 'February 2023 - ...', //timespan
+                description: 'A worldwide community of high school hackers. By the students, for the students.' //eg. spearheaded the graphics team in designing SASS product UIs
             },
             {
-                company: 'Dunder Mifflin',
-                companylogo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*',
-                position: 'Regional Manager',
-                time: 'March 2020 - May 2020',
-                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum'
-            },
-            {
-                company: 'Dunder Mifflin',
-                companylogo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*',
-                position: 'Regional Manager',
-                time: 'March 2020 - May 2020',
-                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum'
+                company: 'the Future',
+                companylogo: 'https://futuree.netlify.app/resimler/futurebanner.png',
+                position: 'Company Owner',
+                time: 'March 2020 - ...',
+                description: 'The Future comes from many parts of the world; was established to gather people with different purposes such as software, coding, programming, domain buying, opening a website, on a single website.'
             },
         ],
     },
